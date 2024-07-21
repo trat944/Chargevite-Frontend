dad098009d93ec158a53c7080493e5f3afc003b9
Jorge Revuelta <jrjrevuelta@gmail.com>
2024-07-21 18:41:06 +0200

    project initialised

---
+++ src/views/About.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>About</h1>
+    <p>Welcome to the about page.</p>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Home',
+};
+</script>
---
+++ src/views/Contact.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Contact</h1>
+    <p>Welcome to the contact page.</p>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Home',
+};
+</script>
