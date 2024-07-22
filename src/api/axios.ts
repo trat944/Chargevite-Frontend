4460c181c0ed2abd0761aad2f24c8d1fabf87a18
Jorge Revuelta <jrjrevuelta@gmail.com>
2024-07-22 09:50:42 +0200

    env files done

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3000', // URL del backend later in env
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
